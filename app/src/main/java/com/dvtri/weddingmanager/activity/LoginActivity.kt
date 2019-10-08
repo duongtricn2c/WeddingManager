4b0edd67c68b473bfdd3dd8410f2fb2c3f58176f
tridv <duongtricn2c@gmail.com>
2019-10-08 23:31:04 +0700

    edit animation Login loading 90%

--- app/src/main/java/com/dvtri/weddingmanager/activity/LoginActivity.kt
+++ app/src/main/java/com/dvtri/weddingmanager/activity/LoginActivity.kt
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import com.dvtri.weddingmanager.R
-import com.dvtri.weddingmanager.fragment.login.FragmentLauncher
 import com.dvtri.weddingmanager.fragment.login.FragmentLogin
-import java.util.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -14,19 +12,9 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        supportFragmentManager.beginTransaction().replace(R.id.containerLogin, FragmentLauncher())
-            .commit()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.containerLogin, FragmentLogin()).commit()
 
-        val timer = object : CountDownTimer(2000, 1000) {
-            override fun onFinish() {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.containerLogin, FragmentLogin()).commit()
-            }
-
-            override fun onTick(p0: Long) {
-            }
-        }
-        timer.start()
     }
 
 }
--- app/src/main/java/com/dvtri/weddingmanager/fragment/login/FragmentLogin.kt
+++ app/src/main/java/com/dvtri/weddingmanager/fragment/login/FragmentLogin.kt
@@ -25,6 +25,11 @@ class FragmentLogin : Fragment(), View.OnClickListener {
 
     var animSlideUp: Animation? = null
     var animSlideDown: Animation? = null
+    var animMoveLeft: Animation? = null
+    var animMoveRight: Animation? = null
+    var animFadeIn : Animation? = null
+    var animScaleUp : Animation? = null
+    var checkFirst : Boolean = true
 
     @SuppressLint("NewApi")
     override fun onClick(v: View?) {
@@ -54,37 +59,83 @@ class FragmentLogin : Fragment(), View.OnClickListener {
 
     private fun initUI() {
         imgLoginFB.setOnClickListener(this)
-        val timer = object : CountDownTimer(200, 200) {
-            override fun onFinish() {
-                tvAccount.startAnimation(animSlideDown)
-                tvPassword.startAnimation(animSlideDown)
+
+        val timer = object  :CountDownTimer(100,100){
+            override fun onTick(p0: Long) {
             }
 
+            override fun onFinish() {
+                imgLogo.startAnimation(animFadeIn)
+            }
+
+        }
+        timer.start()
+
+        val timer2 = object : CountDownTimer(2500, 100) {
+            override fun onFinish() {
+                imgLogo.startAnimation(animScaleUp)
+            }
             override fun onTick(p0: Long) {
             }
         }
-        timer.start()
+        timer2.start()
+
+        val timer3 = object : CountDownTimer(3000, 100) {
+            override fun onFinish() {
+
+                tvAccount.visibility = View.VISIBLE
+                tvPassword.visibility = View.VISIBLE
+                imgLoginFB.visibility = View.VISIBLE
+                imgLoginGG.visibility = View.VISIBLE
+                edtAccount.visibility = View.VISIBLE
+                edtPassword.visibility = View.VISIBLE
+
+                tvAccount.startAnimation(animSlideDown)
+                tvPassword.startAnimation(animSlideDown)
+                imgLoginFB.startAnimation(animMoveLeft)
+                edtAccount.startAnimation(animMoveRight)
+                edtPassword.startAnimation(animMoveRight)
+                imgLoginGG.startAnimation(animMoveRight)
+
+            }
+            override fun onTick(p0: Long) {
+            }
+        }
+        timer3.start()
+
         edtAccount.setOnFocusChangeListener { view, b ->
             if (edtPassword.text.length == 0) {
-                tvPassword.startAnimation(animSlideDown)
+                if (!checkFirst) {
+                    tvPassword.startAnimation(animSlideDown)
+                }
             }
             if ( edtAccount.text.length == 0)
-                tvAccount.startAnimation(animSlideUp)
+                    tvAccount.startAnimation(animSlideUp)
+
+
             else
                 tvAccount.clearAnimation()
+            checkFirst = false
         }
 
         edtPassword.setOnFocusChangeListener { view, b ->
             if (edtAccount.text.length == 0) {
-                tvAccount.startAnimation(animSlideDown)
+                if (!checkFirst) {
+                    tvAccount.startAnimation(animSlideDown)
+                }
             }
             if ( edtPassword.text.length == 0)
                 tvPassword.startAnimation(animSlideUp)
             else
                 tvPassword.clearAnimation()
+            checkFirst = false
         }
         animSlideUp = AnimationUtils.loadAnimation(this.context, R.anim.slide_up)
         animSlideDown = AnimationUtils.loadAnimation(this.context, R.anim.slide_down)
+        animMoveLeft = AnimationUtils.loadAnimation(this.context, R.anim.move_left)
+        animMoveRight = AnimationUtils.loadAnimation(this.context, R.anim.move_right)
+        animFadeIn = AnimationUtils.loadAnimation(this.context, R.anim.fade_in)
+        animScaleUp = AnimationUtils.loadAnimation(this.context, R.anim.scale_up)
     }
 
 
